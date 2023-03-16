be023706851344b693edcb219d116e6ce4b3b2ae
tugcan-kartal <tugcankartal01@gmail.com>
2023-03-16 21:22:59 +0300

    api stuff

--- src/features/HeroView.js
+++ src/features/HeroView.js
@@ -5,16 +5,42 @@ import { fetchCoins } from './heroSlice';
 const HeroView = () => {
 
   const dispatch=useDispatch();
-  let coinData=useSelector((state)=>state.heroReducer);
-  console.log(coinData.coins);
+  let coinData=useSelector((state)=>state.heroReducer).coins;
+  console.log(coinData);
 
   useEffect(()=>{
     dispatch(fetchCoins());
   },[])
 
   return (
-    <div>
+    
+    <div className='h-[90vh]'>
+
+      {coinData.loading && "fetching data"}
+      {coinData.error && "fetching error"}
+      {coinData.length>0 &&
       
+        <div>
+
+          <div>
+            <p>TRACK AND TRADE</p>
+            <p>CRYPTO CURRENCIES</p>
+          </div>
+
+          <div>
+            <div> <img alt='not found' src={coinData[0].image}/> </div>
+            <div>     
+              <div>{coinData[0].name}</div>
+              <div>{coinData[0].price_change_percentage_24h.toFixed(2)}%</div>
+            </div>
+            <div>${coinData[0].current_price}</div>
+          </div>
+
+
+        </div>
+
+      }
+
     </div>
   )
 }
--- src/features/Navbar.js
+++ src/features/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-around items-center gap-x-[20vw] py-[3vh] text-white bg-indigo-900 outline-none border-none'>
+    <div className='flex justify-around items-center gap-x-[20vw] h-[10vh] text-white bg-indigo-900 outline-none border-none'>
         <div className='font-bold text-2xl cursor-pointer'>COIN</div>
         <div className='flex gap-x-[4vw] font-semibold text-lg'>
             <div className='hover:text-purple-500 transition-colors duration-3000 cursor-pointer'>HOME</div>
