6af49af80a32c5b3c589cdf279428fad49f3e519
tugcan-kartal <tugcankartal01@gmail.com>
2023-03-16 17:41:39 +0300

    navbar is done

---
+++ src/features/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-around items-center gap-x-[20vw] py-[3vh] text-white bg-indigo-900 outline-none border-none'>
+        <div className='font-bold text-2xl cursor-pointer'>COIN</div>
+        <div className='flex gap-x-[4vw] font-semibold text-lg'>
+            <div className='hover:text-purple-500 transition-colors duration-3000 cursor-pointer'>HOME</div>
+            <div className='hover:text-purple-500 transition-colors duration-3000 cursor-pointer'>MARKET</div>
+            <div className='hover:text-purple-500 transition-colors duration-3000 cursor-pointer'>CHOOSE US</div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
